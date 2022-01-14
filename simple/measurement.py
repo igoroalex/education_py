202a5bdd427ca66b9f942440320f485d1e2fa710
GoRo <igoroalex@gmail.com>
2022-01-14 18:32:11 +0200

    INIT measurement.py

---
+++ simple/measurement.py
@@ -0,0 +1,29 @@
+from functools import partial
+from timeit import timeit
+
+
+def search_needle1(haystack, needle):
+    for i in haystack:
+        if i == needle:
+            return True
+    return False
+
+
+def search_needle2(haystack, needle):
+    return needle in haystack
+
+
+def search_needle3(haystack, needle):
+    return any((i == needle for i in haystack))
+
+
+search_functions = (search_needle1, search_needle2, search_needle3)
+
+haystack = list(range(10000))
+needle = 9995
+
+n = 10000
+
+for f in search_functions:
+    func_time = timeit(partial(f, haystack, needle), number=n)
+    print(f"{f.__name__} -- {func_time}")
