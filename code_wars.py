d154a545b7002829792eb39d89973a4bc8e7654c
GoRo <igoroalex@gmail.com>
2022-01-21 11:19:56 +0200

    ADD codewars

--- code_wars.py
+++ code_wars.py
@@ -1,13 +1,30 @@
+import re
 from functools import partial
 from timeit import timeit
-from string import punctuation
 
 
-def create_phone_number(numbers):
-    return numbers
+def create_phone_number(n):
+    n = "".join(map(str, n))
+    return f"({n[0:3]}) {n[3:6]}-{n[6:]}"
 
 
-n = 10000
+def create_phone_number1(n):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+
+def create_phone_number2(n):
+    n = "".join(map(str, n))
+    return "(%s) %s-%s" % (n[:3], n[3:6], n[6:])
+
+
+def create_phone_number3(n):
+    return "(%i%i%i) %i%i%i-%i%i%i%i" % tuple(n)
+
+
+def create_phone_number4(n):
+    n_string = "".join(map(str, n))
+    formatted_number = re.sub(r"(\d{3})(\d{3})(\d{4})", r"(\1) \2-\3", n_string)
+    return formatted_number
 
 
 class Test:
@@ -17,10 +34,12 @@ class Test:
         print(text == result)
         search_functions = (
             create_phone_number,
-            create_phone_number,
-            create_phone_number,
-            create_phone_number,
+            create_phone_number1,
+            create_phone_number2,
+            create_phone_number3,
+            create_phone_number4,
         )
+        n = 10000
         for f in search_functions:
             func_time = timeit(partial(f, [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), number=n)
             print(f"{f.__name__} -- {func_time}")
